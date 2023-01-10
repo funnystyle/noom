964840c6a9e25a83c460c5a0ca063b91a7f3d9ed
funnystyle <esero@hanmail.net>
2023-01-11 01:44:24 +0900

    send message to browser

--- src/server.js
+++ src/server.js
@@ -16,7 +16,8 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 wss.on("connection", (socket) => {
-  console.log(socket);
+  console.log("connected to Browser");
+  socket.send("hello!!");
 });
 
 server.listen(3000, handleListen);
