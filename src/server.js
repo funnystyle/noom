ae0b2afbcc92319263892cd65fef598d10b3685a
funnystyle <esero@hanmail.net>
2023-01-12 00:57:29 +0900

    add connection event handler

--- src/server.js
+++ src/server.js
@@ -14,5 +14,9 @@ app.get("/*", (req, res) => res.redirect("/"));
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+wsServer.on("connection", (socket) => {
+  console.log(socket);
+})
+
 const handleListen = () => console.log("Listening on http://localhost:3000");
 httpServer.listen(3000, handleListen);
