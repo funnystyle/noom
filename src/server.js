c43a49225e67f6a5fecd93a62d009763c8aa7b1b
funnystyle <esero@hanmail.net>
2023-01-12 01:03:56 +0900

    add enter_room handler

--- src/server.js
+++ src/server.js
@@ -15,8 +15,8 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-  console.log(socket);
-})
+  socket.on("enter_room", (roomName) => console.log(roomName));
+});
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
 httpServer.listen(3000, handleListen);
