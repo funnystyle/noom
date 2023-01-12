74c0ac850d21eed2ccdcdb04c08e645ec4d94927
funnystyle <esero@hanmail.net>
2023-01-13 00:15:55 +0900

    send count of Room

--- src/server.js
+++ src/server.js
@@ -42,12 +42,12 @@ wsServer.on("connection", (socket) => {
   socket.on("enter_room", (roomName, done) => {
     done();
     socket.join(roomName);
-    socket.to(roomName).emit("welcome", socket.nickname);
+    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
     wsServer.sockets.emit("room_change", publicRooms());
   });
   socket.on("disconnecting", () => {
     socket.rooms.forEach((room) => {
-      socket.to(room).emit("bye", socket.nickname);
+      socket.to(room).emit("bye", socket.nickname, countRoom(roomName) - 1);
     });
   });
   socket.on("disconnect", () => {
